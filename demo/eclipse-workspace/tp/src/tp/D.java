a0a5543d82dc420010646851a23aa9b493aa1b30
mohitgurnani1 <mohitgurnani1@gmail.com>
2016-09-22 08:58:09 +0530

    Till 21 Questions

---
+++ demo/eclipse-workspace/tp/src/tp/D.java
@@ -0,0 +1,13 @@
+package tp;
+
+class D extends C {
+	  void m1() {System.out.print("D");}
+	  void m2() {
+	    m1();
+	    ((C)this).m1(); // 1
+	    ((B)this).m1(); // 2
+	    ((A)this).m1(); // 3
+	  }
+	  public static void main (String[] args) {
+	    new D().m2(); // 4
+	}}
